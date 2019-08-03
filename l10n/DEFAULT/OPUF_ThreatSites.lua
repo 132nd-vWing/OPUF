6d202870250375d72363454ae3a27cce4554a4ef
132nd-Entropy <ber17118@gmail.com>
2019-08-03 15:43:12 +0200

    new: Random Sam Logic

--- l10n/DEFAULT/OPUF_ThreatSites.lua
+++ l10n/DEFAULT/OPUF_ThreatSites.lua
@@ -1,16 +1,130 @@
+--- EVASION COMMANDS ---
 local evasion = true     -- set to false to disable evasive behavior for Radar units
 local chance_for_evasive_action = 85 -- percent chance that the Radar unit will take evasive action
 local prefix = "evade_"  -- all SAM that should be added to the script need to have these word within their GROUP NAME
 local Target_Smoke = false           -- set to TRUE if you want red smoke deployed from the Target SAM  (mainly used for debugging)
-local evasion_delay = math.random(4,8)  -- after HARM launch, delay in seconds until the SAM take evasive action
+local evasion_delay = math.random(4,8)  -- after HARM launch, delay in seconds until the SAM take evasive action (random number between the two)
 local radar_delay = math.random(120,300)   -- time in seconds until the unit will turn its radar back on (random number between the two)
 local move_distance =  math.random(50,150)-- meters the unit will move before stopping (random number between the two)
 local evasion_for_client_planes_only = true -- set to false if Sam Radars should also respond to AI planes firing
 local chance_for_group_relocating = 10   --percent chance that the group shuts down and moves to a new location further away
 local relocating_distance = math.random(150,800)  -- distance in meters that the group relocates
+--- /end EVASION COMMANDS ---
 
 
---- do not change below this line --- 
+--- RANDOMNESS COMMANDS
+local random = true     -- set to false to disable random behavior for Sams
+local prefix_random = "random_"  -- all SAM that should be added to the script need to have these word within their GROUP NAME--- /end RANDOMNESS COMMANDS
+local random_relocating_distance_min = 20  -- distance in meters that the group relocates
+local random_relocating_distance_max = 100  -- distance in meters that the group relocates
+local random_move_chance = 25    --percent chance that the group will mnove after stowing the sensors
+local random_Radar_ON_time_min = 60 -- time in seconds the unit will have the Radar ON (random number between the two)
+local random_Radar_ON_time_max = 180 -- time in seconds the unit will have the Radar ON (random number between the two)
+local random_Radar_Off_time_min = 120 -- time in seconds the unit will have the Radar ON (random number between the two)
+local random_Radar_Off_time_max = 360 -- time in seconds the unit will have the Radar ON (random number between the two)
+
+
+--- do not change below this line ---
+
+
+
+
+-- Random_Sam Script by 132nd.Entropy
+if random == true
+then
+
+
+
+  local RANDOM_enabled_Sams = SET_GROUP:New():FilterPrefixes(prefix_random)
+  RANDOM_enabled_Sams:FilterStart()
+  _random_Sams = {}  -- table that will be filled with all units that are taking random actions
+
+  RANDOM_enabled_Sams:ForEachGroupAlive(
+    function(_group)
+      random_group_name = _group:GetName()
+      env.info("Random Sam function activated for GROUP "..random_group_name)
+      random_units = _group:GetUnits()
+      for i,_unit in ipairs(random_units) do
+        if
+          _unit:HasAttribute("SAM SR") or _unit:HasAttribute("SAM TR")
+        then
+          table.insert(_random_Sams,_unit:GetName())
+        end
+      end
+    end
+  )
+
+
+  function random_radarOFF(_unit)
+    local timer = math.random(random_Radar_Off_time_min,random_Radar_Off_time_max)
+    SCHEDULER:New(nil,
+      function()
+        local targets = _unit:GetDetectedTargets(DetectRadar)
+        if #targets == 0
+        then
+          env.info(_unit:GetName().."has no targets detected by radar ")
+        else
+          env.info(_unit:GetName().."has a radar contact, remaining active ")
+          SCHEDULER:New(nil,
+            function()
+              if _unit:IsActive()
+              then
+                _unit:OptionAlarmStateRed()
+                random_radarON(_unit)
+              end
+            end,{},7)
+        end
+      end,{},timer-4)
+    SCHEDULER:New(nil,
+      function()
+        if _unit:IsActive() then
+          _unit:OptionAlarmStateGreen()
+          env.info(_unit:GetName().." is stowing Sensors ")
+          random_radarON(_unit)
+          Random_Move(_unit)
+        end
+      end,{},timer)
+  end
+
+  function random_radarON(_unit)
+    local timer = math.random(random_Radar_Off_time_min,random_Radar_Off_time_max)
+    SCHEDULER:New(nil,
+      function()
+        if _unit:IsActive() then
+          _unit:OptionAlarmStateRed()
+          env.info(_unit:GetName().." resumes scanning")
+          random_radarOFF(_unit)
+        end
+      end,{},timer)
+  end
+
+
+  for _,_v in ipairs(_random_Sams) do
+    _unit = UNIT:FindByName(_v)
+    if _unit:IsActive() then
+      _unit:OptionAlarmStateRed()
+      env.info(_unit:GetName().." is activating the radar ")
+      random_radarOFF(_unit)
+    end
+  end
+
+
+
+  function Random_Move()
+    local random_relocating_distance = math.random(1,100)
+    if random_relocating_distance <= random_move_chance
+    then
+      if _unit:IsActive() then
+        env.info(_unit:GetName().." is now relocating "..random_relocating_distance.." meters")
+        _unit:OptionAlarmStateGreen()
+        local _groupcoordinate = _unit:GetCoordinate()
+        local _tocoordinate = _groupcoordinate:Translate( math.random(random_relocating_distance_min,random_relocating_distance_max), math.random(359) )
+        local _ToCoord_vec2 = _tocoordinate:GetVec2()
+        _unit:TaskRouteToVec2( _ToCoord_vec2 )
+        _unit:OptionAlarmStateGreen()
+      end
+    end
+  end
 
 
 
@@ -23,6 +137,23 @@ local relocating_distance = math.random(150,800)  -- distance in meters that the
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+-- Evasion_Sam Script by 132nd.Entropy
 
 if evasion == true
 then
@@ -36,13 +167,13 @@ then
   SEAD_enabled_Sams:ForEachGroupAlive(
     function(_group)
       Sam_group_name = _group:GetName()
-      env.info("Sam GROUP Name is "..Sam_group_name)
+      --env.info("Sam GROUP Name is "..Sam_group_name)
       Sam_units = _group:GetUnits()
       for i,_unit in ipairs(Sam_units) do
         if
           _unit:HasAttribute("SAM SR") or _unit:HasAttribute("SAM TR")
         then
-          env.info("Radar detected for unit ".._unit:GetName())
+          env.info("Radar detected for UNIT ".._unit:GetName())
           table.insert(_evadeRadars,_unit:GetName())
         else
         end
